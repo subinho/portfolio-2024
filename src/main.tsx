e2b8dfa816efd178bfba9a8d7fc75a710cf85fea
Stepan Subrt <stepansubrt@icloud.com>
2024-02-05 19:47:05 +0100

    Added react-router-dom and implemented it's newest version

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { About } from "./pages";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
