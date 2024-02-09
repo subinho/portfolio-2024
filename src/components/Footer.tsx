1af05ede53c3c2b717dd2b4e41fb128ec270d93c
Stepan Subrt <stepansubrt@icloud.com>
2024-02-09 13:34:38 +0100

    Added footer, made it compatible with mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,24 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <h3 className="footer_heading">Stepan Subrt Ⓒ 2024</h3>
+      <div className="footer_nav">
+        <ul className="footer_links">
+          <li>
+            <Link to="#hero">Home</Link>
+          </li>
+          <li>
+            <Link to="/About">About</Link>
+          </li>
+          <li>
+            <Link to="#">Resume</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
