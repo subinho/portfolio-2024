a1050b880a1742fb48ae88d22be3ac425ca78cce
Stepan Subrt <stepansubrt@icloud.com>
2024-02-09 13:38:01 +0100

    Edit added a Projects component to the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,29 @@
 import { arrow_down } from "../assets/icons";
+import { Projects } from "../components";
 
 const Home = () => {
   return (
-    <div className="hero">
-      <div className="hero_container">
-        <h2 className="hero_welcome">Hey there, I'm Stepan</h2>
-        <h1 className="hero_heading">
-          A <strong className="hero_strong">Web Developer</strong> passionate
-          about
-          <br /> learning new technologies
-        </h1>
+    <div className="home">
+      {/* hero */}
+      <div className="hero" id="hero">
+        <div className="hero_container">
+          <h2 className="hero_welcome">Hey there, I'm Stepan</h2>
+          <h1 className="hero_heading">
+            A <strong className="hero_strong">Web Developer</strong> passionate
+            about
+            <br className="hero_break" /> learning new technologies
+          </h1>
+        </div>
+        <img
+          src={arrow_down}
+          alt=""
+          className="hero_arrow"
+          height={32}
+          width={32}
+        />
       </div>
-      <img
-        src={arrow_down}
-        alt=""
-        className="hero_arrow"
-        height={32}
-        width={32}
-      />
+      {/* projects */}
+      <Projects />
     </div>
   );
 };
