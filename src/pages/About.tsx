cb14be7d48f74e0b824269bca5646ced056bc13f
Stepan Subrt <stepansubrt@icloud.com>
2024-02-10 01:47:41 +0100

    Edited text about me, to be more readable

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -11,15 +11,15 @@ const About = () => {
           <h1 className="about_heading">About me</h1>
           <p className="about_desc">
             Hello, my name is Stepan, and I am a dedicated software web
-            developer with a strong passion for learning and staying abreast of
-            the latest industry trends.
+            developer with a strong passion for learning and keeping up with the
+            latest trends in the industry.
             <br />
             <br /> I have acquired multiple certifications in web development,
             and I am committed to continuously enhancing my skills. I am
             enthusiastic about the opportunity to contribute to the field of web
             development and am eager to further advance my expertise. <br />
-            <br /> In my leisure time, I indulge in reading about programming
-            and enjoy the occasional video game.
+            <br /> In my free time, I enjoy reading books and programming, as
+            well as playing games.
           </p>
           <button type="button" className="about_btn">
             <a href="mailto:stepansubrt@icloud.com" className="about_link">
