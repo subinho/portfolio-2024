082c7ac3955d0f98d7936a3ee80a3335154abb61
unknown <stepansubrt@icloud.com>
2024-06-25 04:12:55 +0200

    added new project

--- src/assets/data/projectData.ts
+++ src/assets/data/projectData.ts
@@ -1,33 +1,42 @@
-import { under_construction, krasaaduse } from "../images";
-import { ProjectProps } from "../interfaces/global";
+import { under_construction, krasaaduse, quiz_app } from '../images';
+import { ProjectProps } from '../interfaces/global';
 
 const projectData: ProjectProps[] = [
   {
     id: 1,
-    image: under_construction,
-    heading: "E-Commerce",
-    desc: "An online shop which sells shoes",
-    badge: "Next JS",
-    badgeColor: "#000",
-    refer: "#",
+    image: quiz_app,
+    heading: 'Quiz App',
+    desc: 'Series of questions, try to beat yours highest score',
+    badge: 'JavaScript',
+    badgeColor: '#EBD54D',
+    refer: 'https://subinho.github.io/Quiz_app/index.html',
   },
   {
     id: 2,
     image: under_construction,
-    heading: "Book of Recipes",
-    desc: "A website with build in recipes and personal recipes",
-    badge: "JavaScript",
-    badgeColor: "#EBD54D",
-    refer: "#",
+    heading: 'E-Commerce',
+    desc: 'An online shop which sells shoes',
+    badge: 'Next JS',
+    badgeColor: '#000',
+    refer: '#',
   },
   {
     id: 3,
+    image: under_construction,
+    heading: 'Book of Recipes',
+    desc: 'A website with build in recipes and personal recipes',
+    badge: 'JavaScript',
+    badgeColor: '#EBD54D',
+    refer: '#',
+  },
+  {
+    id: 4,
     image: krasaaduse,
-    heading: "Krasa a Duse",
-    desc: "Small bussines page, which offers cosmetic and terapeutic services.",
-    badge: "Wordpress",
-    badgeColor: "#1f9af1",
-    refer: "https://www.krasaaduse.cz",
+    heading: 'Krasa a Duse',
+    desc: 'Small bussines page, which offers cosmetic and terapeutic services.',
+    badge: 'Wordpress',
+    badgeColor: '#1f9af1',
+    refer: 'https://www.krasaaduse.cz',
   },
 ];
 
--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,2 +1,3 @@
-export { default as under_construction } from "./under_construction.jpg";
-export { default as krasaaduse } from "./krasaaduse.webp";
+export { default as under_construction } from './under_construction.jpg';
+export { default as krasaaduse } from './krasaaduse.webp';
+export { default as quiz_app } from './quiz_app.png';
