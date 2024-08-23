a2292424cf47610235154cac9d6421533183e0ca
unknown <stepansubrt@icloud.com>
2024-08-23 19:25:29 +0200

    added new project -> todoapp

--- src/assets/data/projectData.ts
+++ src/assets/data/projectData.ts
@@ -1,9 +1,24 @@
-import { notes, quiz_app, krasaaduse } from '../images';
+import { notes, quiz_app, krasaaduse, todo } from '../images';
 import { ProjectProps } from '../interfaces/global';
 
 const projectData: ProjectProps[] = [
   {
     id: 1,
+    image: todo,
+    heading: 'To-do app',
+    desc: 'A todo web application, you can create, edit, delete and show your tasks, each task has 24 hours to be completed',
+    badges: [
+      { name: 'PHP', bgColor: '#777BB3', color: '#fff' },
+      { name: 'Laravel', bgColor: '#e74430', color: '#fff' },
+      { name: 'SQLite', bgColor: '#00758F', color: '#fff' },
+      { name: 'Tailwind CSS', bgColor: '#38bdf8', color: '#fff' },
+    ],
+    badgeColor: '#EBD54D',
+    refer: '#',
+    github: 'https://github.com/subinho/to-do-assignment',
+  },
+  {
+    id: 2,
     image: notes,
     heading: 'Notes app',
     desc: 'A notes application, where you can create notes which are linked to your account and store them in your database.',
@@ -17,7 +32,7 @@ const projectData: ProjectProps[] = [
     github: 'https://github.com/subinho/notes',
   },
   {
-    id: 2,
+    id: 3,
     image: quiz_app,
     heading: 'Quiz App',
     desc: 'Series of questions, try to beat your highest score.',
@@ -31,7 +46,7 @@ const projectData: ProjectProps[] = [
     github: 'https://github.com/subinho/Quiz_app',
   },
   {
-    id: 3,
+    id: 4,
     image: krasaaduse,
     heading: 'Krasa a Duse',
     desc: 'Small bussines page, which offers cosmetic and terapeutic services.',
--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,4 @@
 export { default as notes } from './notes.png';
 export { default as quiz_app } from './quiz_app.png';
 export { default as krasaaduse } from './krasaaduse.webp';
+export { default as todo } from './todo_app.png';
